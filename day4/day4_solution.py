776208dc44ca09c7d9a1c26c54eed9a1c45c5dcc
7cougar7 <tilogarcia1@gmail.com>
2024-12-04 20:28:39 -0600

    Added Day 4, Part 2

--- day4/day4_solution.py
+++ day4/day4_solution.py
@@ -1,6 +1,9 @@
+def in_range(x, y):
+    return x >= 0 and x < width and y >= 0 and y < height
+
 ## Part 1 Solution ##
 
-combos = [
+xmas_combos = [
     [(0,0), (1,0), (2,0), (3,0)], #straight right
     [(0,0), (-1,0), (-2,0), (-3,0)], #straight left
     [(0,0), (0,-1), (0,-2), (0,-3)], #straight up
@@ -16,19 +19,38 @@ with open('input.txt', 'r') as f:
         grid.append(line.strip().upper())
 
 count = 0
-xmas_constant = "XMAS"
 height = len(grid)
 width = len(grid[0])
 for y in range(height):
     for x in range(width):
-        for indiv_combo in combos:
+        for indiv_combo in xmas_combos:
             word = "".join([
-                grid[y + delta[1]][x + delta[0]]
-                if x + delta[0] >= 0 and x + delta[0] < width and y + delta[1] >= 0 and y + delta[1] < height
-                else ""
+                grid[y + delta[1]][x + delta[0]] if in_range(x + delta[0], y + delta[1]) else ""
                 for delta in indiv_combo
             ])
-            if word == xmas_constant:
+            if word == "XMAS":
                 count += 1
 
 print("Day 4, Part 1 Answer: " + str(count))
+
+
+## Part 2 Solution ##
+
+match = ['M', 'S']
+count_x_mas = 0
+for y in range(height):
+    for x in range(width):
+        if grid[y][x] != 'A':
+            continue
+        t2b_diag = sorted((
+            grid[y+1][x+1] if in_range(y+1, x+1) else "",
+            grid[y-1][x-1] if in_range(y-1, x-1) else ""
+        ))
+        b2t_diag = sorted((
+            grid[y-1][x+1] if in_range(y-1, x+1) else "",
+            grid[y+1][x-1] if in_range(y+1, x-1) else ""
+        ))
+        if t2b_diag == match and b2t_diag == match:
+            count_x_mas += 1
+
+print("Day 4, Part 2 Answer: " + str(count_x_mas))
